08bcfceaa9a156b2398c476f7bc7bc97cbdafadb
cchura94 <cchura.cpc@gmail.com>
2021-06-01 20:14:04 -0400

    LOGIN VALIDACIONES

--- src/components/templates/PlantillaPagina.vue
+++ src/components/templates/PlantillaPagina.vue
@@ -1,6 +1,29 @@
 <template>
     <div>
-        <h1>Plantlla Pagina Web </h1>
+        <v-app-bar
+            color="deep-purple accent-4"
+            
+            dark
+            >
+
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+            <v-toolbar-title>EMPRESA</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn text to="/">
+            INICIO
+                <v-icon>mdi-home</v-icon>
+            </v-btn>
+
+            <v-btn text to="/login">
+                INGRESAR
+                <v-icon>mdi-account</v-icon>
+            </v-btn>
+
+
+        </v-app-bar>
+
 
         <router-view></router-view>
     </div>
--- src/views/pagina/Login.vue
+++ src/views/pagina/Login.vue
@@ -1,16 +1,65 @@
 <template>
-    <div>
-        <h1>Login </h1>
-        {{ usuario }}
-        <br>
+    <v-container>
+        <v-row align="center" justify="center">
+            <v-col cols="12" sm="8" md="4">
+                <v-card class="elevation-12">
+                    <v-toolbar color="primary" dark flat>
+                        <v-toolbar-title>INGRESAR</v-toolbar-title>
 
-        <input type="email" v-model="usuario.email">
-        <br>
-        <input type="password" v-model="usuario.password">
-        <br>
-        <v-btn @click="ingresar()">Ingresar</v-btn>
+                    </v-toolbar>
 
-    </div>
+                     <v-card-text>
+                        <v-form ref="form" v-model="valid">
+                            <v-text-field
+                                label="Ingrese su correo"
+                                name="login"
+                                prepend-icon="mdi-account"
+                                type="email"
+                                v-model="usuario.email"
+                                :counter="50"
+                                :rules="correoRules"
+
+                            ></v-text-field>
+        
+                            <v-text-field
+                                id="password"
+                                label="Ingrese su Contraseña"
+                                name="password"
+                                prepend-icon="mdi-lock"
+                                type="password"
+                                v-model="usuario.password"
+                                :rules="[v => !!v || 'La Contraseña es Obligatoria']"
+                                ></v-text-field>
+                        </v-form>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="primary" @click="ingresar()" :disabled="!valid">Ingresar</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
+
+        <v-snackbar
+            v-model="snackbar"
+            :timeout="timeout"
+            :color="color"
+            >
+            {{ text }}
+
+            <template v-slot:action="{ attrs }">
+                <v-btn
+                color="blue"
+                text
+                v-bind="attrs"
+                @click="snackbar = false"
+                >
+                Cerrar
+                </v-btn>
+            </template>
+            </v-snackbar>
+
+    </v-container>
 </template>
 
 <script>
@@ -22,14 +71,39 @@ export default {
             usuario: {
                 email: '',
                 password: ''
-            }
+            },
+             snackbar: false,
+             color: "green",
+            text: 'Credenciales Incorrectas',
+            timeout: 2000,
+            correoRules: [
+                v => !!v || 'El Correo es Obligatorio',
+                v => (v && v.length <= 40) || 'El correo debe ser menor a 40 Caracteres',
+                v => /.+@.+\..+/.test(v) || 'El correo no es Valido',
+            ],
+            valid: true
         }
     },
     methods: {
         async ingresar(){
+
+            this.validate();
+
             let { data } = await axios.post("http://127.0.0.1:3000/api/auth/login2", this.usuario)
             console.log(data);
-        }
+            if(!data.error){
+                this.text = "Bienvenido Usuario"
+                this.snackbar = true
+                this.color = "green"
+            }else{
+                this.text = data.mensaje
+                this.snackbar = true
+                this.color = "red"
+            }
+        },
+        validate () {
+            this.$refs.form.validate()
+        },
     }
 }
 </script>
